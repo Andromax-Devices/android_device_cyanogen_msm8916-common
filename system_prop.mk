a3188a59593a88473ba6eda784386567591a29d7
Nicholas Lim <nicknitewolf@lineageos.org>
2021-05-26 21:30:12 +0800

    msm8916-common: Force disable display partial updates
    Postprocessing daemon (ppd) will only work when partial
    updates is in a disabled state.
    Logic for checking dynamic partial updates state via
    the sysfs node (/sys/class/graphics/fb0/dyn_pu) seems
    to be broken in ppd as it keeps polling and causing
    100% cpu utilization:
    
    openat(AT_FDCWD, "/sys/class/graphics/fb0/dyn_pu", O_RDONLY) = 6
    ppoll([{fd=4096, events=0}, {fd=6, events=POLLPRI|POLLERR}], 2, NULL, NULL, 0) = 1 ([{fd=0, revents=POLLIN}])
    ppoll([{fd=4096, events=0}, {fd=6, events=POLLPRI|POLLERR}], 2, NULL, NULL, 0) = 1 ([{fd=0, revents=POLLIN}])
    ppoll([{fd=4096, events=0}, {fd=6, events=POLLPRI|POLLERR}], 2, NULL, NULL, 0) = 1 ([{fd=0, revents=POLLIN}])
    
    For now, we will disable partial updates until the
    poll loop is solved.
    
    Change-Id: I0616d5dca816213274eeea5746c48973e6d770c5
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- system_prop.mk
+++ system_prop.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     persist.hwc.mdpcomp.enable=true \
-    persist.hwc.ptor.enable=true
+    persist.hwc.ptor.enable=true \
+    persist.hwc.pubypass=true
 
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
